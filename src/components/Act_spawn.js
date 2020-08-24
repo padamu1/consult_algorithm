7167aa91cb6daa8381c4c51d7b59a1328a6d8e70
padamu1 <padamu1@naver.com>
2020-08-24 23:39:51 +0900

    modeling

--- src/components/Act_spawn.js
+++ src/components/Act_spawn.js
@@ -1,15 +1,15 @@
 import React from 'react';
   export default function Act_spawn(w_spawn) {
-    switch(w_spawn){
-        case w_entity:
+    switch(w_spawn){ // str compare
+        case "w_entity": //case
             return <span><>w_entity</></span>
-        case w_arrows:
+        case "w_arrows":
             return <span><>w_arrows</></span>
-        case w_switch:
+        case "w_switch":
             return <span><>w_switch</></span>
-        case w_onoff:
+        case "w_onoff":
             return <span><>w_onoff</></span>
-        case w_panel:
+        case "w_panel":
             return <span><>w_panel</></span>
         
     
